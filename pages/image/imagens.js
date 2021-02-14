1b509c0fef00d8f49209245d8602a18c3a85ed4e
Tarcisio Blois <tarcisio.b.neto@gmail.com>
2021-02-14 02:07:01 -0300

    bugs deploys fixes

--- pages/image/imagens.js
+++ pages/image/imagens.js
@@ -8,4 +8,14 @@ function Tempo2() {
   return <Image src="/tempo2.jpg" alt="1" width="564" height="564" />;
 }
 
+function ShowImagens(Asas, Tempo2) {
+  return (
+    <div>
+      <Asas />
+      <Tempo2 />
+    </div>
+  );
+}
+
 export { Asas, Tempo2 };
+export default ShowImagens;
